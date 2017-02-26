2345fec3af1814c4a5d51b5d3a0df8c8cb43ba90
Buğra Dönmez <bugradnmz@hotmail.com>
2017-02-26 15:11:06 +0300

    Create LevelManager.cs

---
+++ LevelManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		Debug.Log ("New Level load: " + name);
+		Brick.breakableCount = 0;
+		Ball.hasGameStarted = false;
+		Application.LoadLevel (name);
+	}
+
+	public void QuitRequest(){
+		Debug.Log ("Quit requested");
+		Application.Quit ();
+	}
+
+	public void LoadNextLevel(){
+		Brick.breakableCount = 0;
+		Ball.hasGameStarted = false;
+		Application.LoadLevel (Application.loadedLevel + 1);
+	}
+	
+	public void BrickDestroyed(){
+		if(Brick.breakableCount <= 0){
+			LoadNextLevel();
+		}
+	}
+}
